6e23867bfa63523075bbb20ad88e982c54a96a80
zecawuw <88804350+zecawuw@users.noreply.github.com>
2023-04-27 16:18:07 -0300

    Add files via upload

---
+++ js/pages.js
@@ -0,0 +1,23 @@
+function AReceber(){
+    location.href = "a_receber.html"
+}
+
+function historicoHolerite(){
+    location.href = "historico_holerite.html"
+}
+
+function historicoSalario(){
+    location.href = "em_desenvolvimento.html"
+}
+
+function pontoEletronico(){
+    location.href = "em_desenvolvimento.html"
+}
+
+function ferias(){
+    location.href = "em_desenvolvimento.html"
+}
+
+function documentos(){
+    location.href = "em_desenvolvimento.html"
+}
