9ec62130f99e12fd2bebc2ee5df0abe08ff17da1
zecawuw <88804350+zecawuw@users.noreply.github.com>
2023-04-27 14:16:14 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -2,9 +2,11 @@ function logar(){
 
     var login = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
-
-    if(login == "admin" && senha == "admin"){
+    
+    if(login == "funcionario" && senha == "admin"){
         location.href = "home_funcionario.html"
+    }else if(login == "rh" && senha == "admin"){
+        location.href = "home_rh.html"
     }else{
         alert("Usúario ou Senha incorretos")
     }
