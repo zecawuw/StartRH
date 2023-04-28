9653a165c2b7f729eac7ee6d6b144f8430698029
zecawuw <88804350+zecawuw@users.noreply.github.com>
2023-04-28 17:44:44 -0300

    Add files via upload

--- js/a_receber.js
+++ js/a_receber.js
@@ -1,5 +1,21 @@
-/*var valor = document.getElementById("i");
+var previstas = document.getElementById("previstas");
+previstas.innerHTML = "180";
 
-valor.innerHTML = "Hello World!";*/
+var trabalhadas = document.getElementById("trabalhadas");
+trabalhadas.innerHTML = "180";
 
-document.getElementById("i").innerHTML = "Hello World";
+var bruto = document.getElementById("bruto");
+bruto.innerHTML = "R$ 1800,00";
+
+var inss = document.getElementById("inss");
+inss.innerHTML = "R$ 162,00";
+
+var ir = document.getElementById("ir");
+ir.innerHTML = "R$ 135,00";
+
+var desconto = document.getElementById("desconto");
+desconto.innerHTML = "R$ 297,00";
+
+var liquido = document.getElementById("liquido");
+liquido.innerHTML = "R$ 1503,00";
+
