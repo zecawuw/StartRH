2f0a8010c741f1d193f1acdf9b18c3a67da1e6bb
zecawuw <88804350+zecawuw@users.noreply.github.com>
2023-09-27 11:08:00 -0300

    Add files via upload

--- js/pages.js
+++ js/pages.js
@@ -1,57 +1,57 @@
 function AReceber(){
-    location.href = "../html/a_receber.html"
+    location.href = "a_receber.html"
 }
 
 function historicoHolerite(){
-    location.href = "../html/historico_holerite.html"
+    location.href = "historico_holerite.html"
 }
 
 function historicoSalario(){
-    location.href = "../html/em_desenvolvimento.html"
+    location.href = "em_desenvolvimento.html"
 }
 
 function pontoEletronico(){
-    location.href = "../html/em_desenvolvimento.html"
+    location.href = "em_desenvolvimento.html"
 }
 
 function ferias(){
-    location.href = "../html/em_desenvolvimento.html"
+    location.href = "em_desenvolvimento.html"
 }
 
 function documentos(){
-    location.href = "../html/em_desenvolvimento.html"
+    location.href = "em_desenvolvimento.html"
 }
 
 function home(){
-    location.href = "../html/home_funcionario.html"
+    location.href = "home_funcionario.html"
 }
 
 function homeRH(){
-    location.href = "../html/home_rh.html"
+    location.href = "home_rh.html"
 }
 
 function Gfuncionarios(){
-    location.href = "../html/controle_funcionario.html"
+    location.href = "controle_funcionario.html"
 }
 
 function Gempresas(){
-    location.href = "../html/controle_empresas.html"
+    location.href = "controle_empresas.html"
 }
 
 function agendar(){
-    location.href = "../html/agendar_pagamento.html"
+    location.href = "agendar_pagamento.html"
 }
 
 function Hpagamento(){
-    location.href = "../html/historico_pagamento.html"
+    location.href = "historico_pagamento.html"
 }
 
 function Gponto(){
-    location.href = "../html/em_desenvolvimento_rh.html"
+    location.href = "em_desenvolvimento_rh.html"
 }
 
 function Gferias(){
-    location.href = "../html/em_desenvolvimento_rh.html"
+    location.href = "em_desenvolvimento_rh.html"
 }
 
 function menu(){
